d2462c01b4ef2ff59802fc535adc11e6ef681f8e
dzonib <nikolabosnjak381@gmail.com>
2019-05-13 09:18:38 +0200

    added rooms container, displaying all rooms next and sorting them out

--- src/Components/RoomsContainer.js
+++ src/Components/RoomsContainer.js
@@ -1,2 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
+import { RoomContext } from '../context'
+import RoomsFilter from './RoomsFilter'
+import RoomList from './RoomList'
+import Loading from './Loading'
+
+export default function RoomsContainer() {
+    const { loading, sortedRooms, rooms } = useContext(RoomContext)
+
+    console.log(loading)
+    
+    if (loading) {
+        return <Loading />
+    } else {
+        return (
+            <div>
+                helloo from rooms container
+                <RoomsFilter rooms={rooms} />
+                <RoomList rooms={sortedRooms} />
+            </div>
+        )
+    }
+}
